d4e33bdc9660af6056221cd2d1697c1692d12b71
Siddikur-22 <designsiddik@gmail.com>
2024-05-29 00:52:12 +0600

    added the reftch

--- index.js
+++ index.js
@@ -37,14 +37,23 @@ async function run() {
         const cartCollection = client.db("BistroDp").collection("carts");
 
         // 1st data load
-        app.get('/menu', async(req, res) => {
+        app.get('/menu', async (req, res) => {
             const result = await MenuCollection.find().toArray();
             res.send(result)
         })
 
+
+        // tanstack function 
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const result = await cartCollection.find(query).toArray();
+            res.send(result);
+        })
+
         // carts collection
 
-        app.post('/carts',async(req,res)=>{
+        app.post('/carts', async (req, res) => {
             const cartItem = req.body;
             const result = await cartCollection.insertOne(cartItem);
             res.send(result);
